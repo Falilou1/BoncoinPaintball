915b93b24a35a57541cf872dcb6aa999deba58f1
Falilou1 <falou40@gmail.com>
2023-06-13 07:15:31 +0200

    mise en place du systeme d'auth & register, de la page de profil d'un user, du filtre de recherche et des pages d'accueil, d'affichage des annonces

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -56,28 +56,30 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->add($user, true);
     }
 
-//    /**
-//     * @return User[] Returns an array of User objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return User[] Returns an array of User objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('u.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?User
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findOneBySomeField(string $postalCode): array
+    {
+        $query = $this->createQueryBuilder('u');
+
+        if ($postalCode != null) {
+            $query->andWhere('u.postalCode = :postalCode')
+                ->setParameter('postalCode', $postalCode);
+        }
+        $query->orderBy('u.postalCode', 'ASC');
+        return (array)$query->getQuery()->getResult();
+    }
 }
